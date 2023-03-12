2bf77a2ff7fbfb3b726d11be993ccd93cbd90035
nayeem-rafsan <nayeem.rafsan@bkash.com>
2023-03-12 11:54:48 +0600

    Add Navbar in Homepage

--- surelink-vue/src/components/HelloWorld.vue
+++ surelink-vue/src/components/HelloWorld.vue
@@ -1,33 +1,27 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <h2 class="title">Shorten Your URLS</h2>
+        <h3>Transforming long, ugly links into Shorten URLs</h3>
+        <div class="columns is-centered">
+        <div class="card column is-8">
+          <div class="card-content">
+            <div class="content">
+                <h4 class="subtitle"> Shorten URL Is Just Simple</h4>
+                <div class="field">
+              <div class="control is-medium is-centered columns is-variable is-4">
+                <span class="column is-6">
+                    <input class="input is-medium" type="text" placeholder="Input your ugly link, we'll do the rest">
+                </span>
+                <span class="column is-2">
+                    <button class="button is-rounded is-medium is-primary">Shorten</button>
+                </span>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -41,8 +35,14 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+h2{
+  margin: 40px 0 0 0;
+}
 h3 {
-  margin: 40px 0 0;
+  margin: 0 0 40px 0;
+}
+.card {
+    height: 400px;
 }
 ul {
   list-style-type: none;
--- surelink-vue/src/main.js
+++ surelink-vue/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+require('@/assets/css/main.scss');
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
 
