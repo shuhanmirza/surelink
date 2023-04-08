e49b434254fb9b44091def8cad7b7dc7b0075e67
rafu01 <nrafsan13@gmail.com>
2023-04-09 00:16:31 +0600

    Add Body, Footer in Homepage & Remove unwanted items

---
+++ surelink-vue/src/components/FooterItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer class="footer">
+      <div class="content has-text-centered">
+        <p>Our easy-to-use URL shortener allows you to quickly and easily shorten long URLs into custom, easy-to-remember links that you can share with your audience.</p>
+        <p>More Exciting Features Coming Soon!</p>
+        <p>© SureLink 2023</p>
+      </div>
+    </footer>
+</template>
+<script>
+export default {
+    name: 'FooterItem'
+}
+</script>
+<style scoped lang="scss">
+    .footer {
+      background-color: #2C3333;
+      color: #fff;
+      padding: 20px;
+      margin-top: 20px;
+    }
+</style>
--- surelink-vue/src/components/Navbar.vue
+++ surelink-vue/src/components/Navbar.vue
@@ -2,36 +2,8 @@
   <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
     <div class="navbar-brand columns">
       <a class="column is-3" href="/">
-        <img src="https://cdn-icons-png.flaticon.com/512/1011/1011407.png" width="28" height="28"> SureLink
+        <img src="../../src/assets/images/logo.svg" width="32" height="32"> SureLink
       </a>
-      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-      <div class="navbar-menu column is-6 content" id="navbarBasicExample">
-        <div class="columns">
-          <a class="column">
-            Home
-          </a>
-          <a class="column">
-            About
-          </a>
-          <a class="column">
-            Contact
-          </a>
-        </div>
-      </div>
-      <div class="navbar-end navbar-item column is-3">
-        <div class="buttons">
-          <button class="button is-rounded is-primary">
-            Sign up
-          </button>
-          <button class="button is-rounded is-light">
-            Log in
-          </button>
-        </div>
-      </div>
     </div>
   </nav>
 </template>
