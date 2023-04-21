39887c99ffda3bb0679111880fbea1a80656480f
rafu01 <nrafsan13@gmail.com>
2023-04-21 12:42:24 +0600

    Add toast notification for success and error

--- surelink-vue/src/components/HomeItem.vue
+++ surelink-vue/src/components/HomeItem.vue
@@ -32,9 +32,22 @@
                   </span>
                 </div>
               </div>
-              <div v-if="img">
+              <div v-if="!isVerified && img && showDiv">
                   <img :src="'data:image/png;base64,' + img" alt="Base64 Image"/> <br/>
-                  <input class="input is-rounded is-primary is-small captcha-input" type="text" v-model="captchaValue"/> <br/>
+<!--                  <button class="button"> .-->
+<!--                      <img src="../assets/icons/rotate-right-solid.svg" alt="reload"/>-->
+<!--                  </button>-->
+                  <input class="input is-rounded is-primary is-small captcha-input" type="text" v-model="captchaValue" placeholder="Enter Captcha Value"/> <br/>
+              </div>
+              <div class="columns is-centered" v-if="isVerified && success">
+                <span class="column is-5">
+                    <input class="input is-medium" ref="shortenField" v-model="shortenUrl" type="text"/>
+                </span>
+                <span class="column is-2">
+                  <button class="button is-rounded is-medium is-small" @click="copyLink">
+                    Copy
+                  </button>
+                </span>
               </div>
               <br />
               <div class="columns is-centered">
@@ -61,25 +74,19 @@
           </div>
         </div>
       </div>
-        <div class="columns is-centered" v-if="success">
-            <span class="column is-5">
-                <input class="input is-medium" ref="shortenField" v-model="shortenUrl" type="text" disabled/>
-            </span>
-            <span class="column is-2">
-                <button class="button is-rounded is-medium is-small" @click="copyLink">
-                  Copy
-                </button>
-            </span>
-        </div>
     </div>
   </div>
 </template>
 
 <script>
 
+import Vue from 'vue';
 import VueJsCounter from 'vue-js-counter';
 import axios from 'axios';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
+Vue.use(Toast);
 export default {
   name: "HomeItem",
   components: { VueJsCounter },
@@ -91,44 +98,64 @@ export default {
           captchaValue: '',
           targetLink: '',
           shortenUrl: '',
-          success: false
+          success: false,
+          showDiv: true
       };
   },
   mounted() {
-      axios.get('https://api.surel.ink/captcha/new')
-          .then(response => {
-              this.uuid = response.data['uuid'];
-              this.img = response.data['img'];
-          })
-          .catch(error => {
-              console.log(error);
-          });
+      this.generateCaptcha();
   },
   methods: {
+      generateCaptcha(){
+          axios.get('https://api.surel.ink/captcha/new')
+              .then(response => {
+                  this.uuid = response.data['uuid'];
+                  this.img = response.data['img'];
+              })
+              .catch(error => {
+
+              });
+      },
       verifyCaptcha(captcha, link) {
           const requestBody = {
               "captcha_uuid": this.uuid,
               "captcha_value": captcha,
               "url": link
           }
-          console.log(requestBody);
           axios.post('https://api.surel.ink/redirection/set-map', requestBody)
               .then(response => {
                   this.shortenUrl = response.data['short_url'];
                   this.success = true;
+                  this.isVerified = true;
+                  this.toastSuccess('Link Generated!');
               })
               .catch(error => {
-                  console.log(error);
+                  this.toastFailure('Wrong Captcha, Please Retry');
+                  this.generateCaptcha();
               });
-      },
-      showError(){
-
       },
       copyLink(){
           const inputField = this.$refs.shortenField;
-          inputField.select();
-          document.execCommand("copy");
+          navigator.clipboard.writeText(inputField.value);
           inputField.setSelectionRange(0, 0);
+          this.toastSuccess('Link copied to clipboard!');
+      },
+      toastSuccess(message){
+          this.$toast.success(message, {
+              timeout: 2000, // duration of the toast message in milliseconds
+              position: 'bottom-center' // position of the toast message on the screen
+          });
+      },
+      toastFailure(message){
+          this.$toast.error(message, {
+              timeout: 2000, // duration of the toast message in milliseconds
+              position: 'bottom-center' // position of the toast message on the screen
+          });
+      }
+  },
+  watch: {
+      success: function(newValue, oldValue) {
+          this.showDiv = false;
       }
   }
 }
