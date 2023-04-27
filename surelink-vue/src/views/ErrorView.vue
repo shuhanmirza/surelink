a766dfcd7623275cd90d20960b4c3fff5bd058d7
rafu01 <nrafsan13@gmail.com>
2023-04-27 16:25:37 +0600

    Create 404 error page

---
+++ surelink-vue/src/views/ErrorView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="error">
+        <ErrorItem/>
+    </div>
+</template>
+
+<script>
+import ErrorItem from "@/components/ErrorItem.vue";
+
+export default {
+name: "ErrorView",
+    components: {ErrorItem}
+}
+</script>
+
+<style scoped>
+
+</style>
