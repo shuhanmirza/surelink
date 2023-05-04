33998288e46d44e5df740251394cd91834855e7b
rafu01 <nrafsan13@gmail.com>
2023-05-04 22:09:06 +0600

    Remove comments and remove unnecessary code

--- surelink-vue/src/views/RedirectView.vue
+++ surelink-vue/src/views/RedirectView.vue
@@ -1,71 +1,71 @@
-<!--<template>-->
-<!--    <div id="counter">-->
-<!--        <CountDown/>-->
-<!--    </div>-->
-<!--</template>-->
+<template>
+    <div id="counter">
+        <CountDown/>
+    </div>
+</template>
 
-<!--<script>-->
+<script>
 
-<!--import Vue from 'vue';-->
-<!--import CountDown from "../components/CountDown.vue";-->
-<!--import axios from "axios";-->
-<!--import Toast from "vue-toastification";-->
-<!--import "vue-toastification/dist/index.css";-->
+import Vue from 'vue';
+import CountDown from "../components/CountDown.vue";
+import axios from "axios";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
 
-<!--Vue.use(Toast);-->
-<!--export default {-->
-<!--    name: "RedirectView",-->
-<!--    components: {-->
-<!--        CountDown-->
-<!--    },-->
-<!--    mounted() {-->
-<!--        const link = this.$route.params.link;-->
-<!--        let redirectLink = '';-->
+Vue.use(Toast);
+export default {
+    name: "RedirectView",
+    components: {
+        CountDown
+    },
+    mounted() {
+        const link = this.$route.params.link;
+        let redirectLink = '';
 
-<!--        axios.get('https://api.surel.ink/redirection/get-map', {-->
-<!--            params: {-->
-<!--                "uid": link-->
-<!--            },-->
-<!--            headers: {-->
-<!--                'Content-Type': 'application/json'-->
-<!--            },-->
-<!--        })-->
-<!--            .then(response => {-->
-<!--                redirectLink = response.data['url'];-->
-<!--            })-->
-<!--            .catch(error => {-->
-<!--                console.log(error.response);-->
-<!--            });-->
+        axios.get('https://api.surel.ink/redirection/get-map', {
+            params: {
+                "uid": link
+            },
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(response => {
+                redirectLink = response.data['url'];
+            })
+            .catch(error => {
+                console.log(error.response);
+            });
 
-<!--        setTimeout(()=> {-->
-<!--            if (redirectLink !== '') {-->
-<!--                window.location.href = redirectLink;-->
-<!--            }-->
-<!--            else {-->
-<!--                this.$toast.error('Incorrect Link, Unable to redirect', {-->
-<!--                    timeout: 2000,-->
-<!--                    position: 'bottom-center'-->
-<!--                });-->
-<!--            }-->
-<!--        }, 5500);-->
-<!--    }-->
-<!--};-->
-<!--</script>-->
+        setTimeout(()=> {
+            if (redirectLink !== '') {
+                window.location.href = redirectLink;
+            }
+            else {
+                this.$toast.error('Incorrect Link, Unable to redirect', {
+                    timeout: 2000,
+                    position: 'bottom-center'
+                });
+            }
+        }, 5500);
+    }
+};
+</script>
 
-<!--<style>-->
-<!--body {-->
-<!--    background-color: #fff;-->
-<!--}-->
+<style>
+body {
+    background-color: #fff;
+}
 
-<!--#counter {-->
-<!--    -webkit-font-smoothing: antialiased;-->
-<!--    -moz-osx-font-smoothing: grayscale;-->
-<!--    text-align: center;-->
-<!--    margin-top: 60px;-->
-<!--    display: flex;-->
-<!--    flex-direction: column;-->
-<!--    justify-content: center;-->
-<!--    align-items: center;-->
-<!--    width: 100%;-->
-<!--}-->
-<!--</style>-->
+#counter {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin-top: 60px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+</style>
