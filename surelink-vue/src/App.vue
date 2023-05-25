44907d749ff1244721d05346348a0a7426d4b872
rafu01 <nrafsan13@gmail.com>
2023-05-25 21:32:02 +0600

    Add meta tags for SEO

--- surelink-vue/src/App.vue
+++ surelink-vue/src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="app">
+    <meta name="title" content="SureLink - Free Link Shortener" />
+    <meta name="description" content="It is a short link generator tool that helps you generate tinyurls. It provides security from harmful links and makes the urls human friendly" />
+    <meta name="keywords" content="url shortener free, bitly url shortener, google short url, tinyurl generator, url shorten" />
+    <meta property="og:title" content="SureLink - Free Link Shortener" />
+    <meta property="og:description" content="It is a short link generator tool that helps you generate tinyurls. It provides security from harmful links and makes the urls human friendly" />
+    <meta property="og:image" content="https://surel.ink/img/logo.b4c96294.svg" />
     <router-view/>
   </div>
 </template>
