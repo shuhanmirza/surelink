895370eb09224ee0467d064ee717be9c8cf3fa68
rafu01 <nrafsan13@gmail.com>
2023-05-25 21:32:02 +0600

    Add meta tags for SEO

--- surelink-vue/src/components/Navbar.vue
+++ surelink-vue/src/components/Navbar.vue
@@ -1,38 +1,37 @@
 <template>
-  <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand columns">
-      <a class="column is-3" href="/">
-        <img src="../../src/assets/images/logo.svg" width="32" height="32"> SureLink
-      </a>
-    </div>
-  </nav>
+    <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a href="/">
+                <img src="../../src/assets/images/logo.svg" width="32" height="32"> SureLink
+            </a>
+        </div>
+    </nav>
 </template>
 <script>
 export default {
-  name: 'Navbar'
+    name: 'Navbar'
 }
 </script>
 <style>
 .navbar .navbar-brand {
-  text-align: center;
-  display: block;
-  width: 100%;
+    text-align: center;
+    display: block;
+    width: 100%;
 }
 
 nav.navbar {
-  min-height: unset;
-  height: 0;
-  font-family: 'Roboto';
-  padding: 0;
-
+    min-height: unset;
+    height: 0;
+    font-family: 'Roboto';
+    padding: 0;
 }
 
 a {
-  text-decoration: none !important;
+    text-decoration: none !important;
 }
 
-.navbar .navbar-brand>.navbar-item,
+.navbar .navbar-brand > .navbar-item,
 .navbar .navbar-brand {
-  display: inline-block;
+    display: inline-block;
 }
 </style>
