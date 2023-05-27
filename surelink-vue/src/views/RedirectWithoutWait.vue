5e0448044ecba4bbd1c295bf8211a7580113ee34
rafu01 <nrafsan13@gmail.com>
2023-05-27 14:33:18 +0600

    Change card design and add logo on top

--- surelink-vue/src/views/RedirectWithoutWait.vue
+++ surelink-vue/src/views/RedirectWithoutWait.vue
@@ -1,13 +1,18 @@
 <template>
-    <PreviewRedirection/>
+    <div>
+        <Navbar/>
+        <PreviewRedirection/>
+    </div>
 </template>
 
 <script>
 import PreviewRedirection from "@/components/PreviewRedirection.vue";
+import Navbar from "@/components/Navbar.vue";
+import FooterItem from "@/components/FooterItem.vue";
 
 export default {
     name: "RedirectWithoutWait.vue",
-    components: {PreviewRedirection}
+    components: {FooterItem, Navbar, PreviewRedirection}
 }
 </script>
 
